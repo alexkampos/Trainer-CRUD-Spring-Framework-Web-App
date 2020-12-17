faa4606ec4d86826502736e29dd237b47930fb9d
Alex K <alexkambos@gmail.com>
2020-12-17 09:05:22 +0200

    Update trainercrudqueries.sql

--- trainercrudqueries.sql
+++ trainercrudqueries.sql
@@ -1,9 +1,10 @@
-create database trainers;
-create table trainers(
-trainer_id int not null auto_increment,
-first_name varchar(30) not null,
-last_name varchar(30) not null,
-age int not null,
-primary key (trainer_id),
-unique (first_name,last_name,subject)
+CREATE DATABASE trainers;
+USE trainers;
+CREATE TABLE trainers(
+trainer_id int NOT NULL AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_name varchar(30) NOT NULL,
+age int NOT NULL,
+PRIMARY KEY (trainer_id),
+UNIQUE (first_name,last_name,subject)
 );
