87721d0fd1c85cb1cfc1934a18a9271b2b867556
Alex K <alexkambos@gmail.com>
2020-12-17 09:05:59 +0200

    Update trainercrudqueries.sql

--- trainercrudqueries.sql
+++ trainercrudqueries.sql
@@ -3,7 +3,7 @@ USE trainers;
 CREATE TABLE trainers(
 trainer_id int NOT NULL AUTO_INCREMENT,
 first_name VARCHAR(30) NOT NULL,
-last_name varchar(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
 age INT NOT NULL,
 PRIMARY KEY (trainer_id),
 UNIQUE (first_name,last_name,subject)
